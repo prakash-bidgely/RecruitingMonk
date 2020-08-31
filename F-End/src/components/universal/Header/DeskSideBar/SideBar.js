ef1b8ab375455f63813c72a0fce3a44a95ea343c
mvermadev <manishverma001997@gmail.com>
2020-08-31 12:40:53 +0530

    mnr

--- F-End/src/components/universal/Header/DeskSideBar/SideBar.js
+++ F-End/src/components/universal/Header/DeskSideBar/SideBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MenuIcon from '@material-ui/icons/Menu';
 import LiveHelpIcon from '@material-ui/icons/LiveHelp';
 import MailIcon from '@material-ui/icons/Mail';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
 import {useHistory} from 'react-router-dom'
 import './DeskSide.css'
@@ -20,6 +21,11 @@ function SideBar() {
 
     }
 
+    const Logout=()=>{
+        localStorage.removeItem('token');
+        history.push('/')
+    }
+
     return (
         <div>
             <div id="mySidenav" className="sidenav" onMouseOver={OpenNav} onMouseOut={CloseNav}>
@@ -36,7 +42,17 @@ function SideBar() {
             <LocalLibraryIcon style={{color: '#b32800'}} fontSize="medium" />
             <p onClick={()=>history.push('/articles')}>Library</p>
             </div>
+            {
+                localStorage.getItem('token') ? 
+                <div className="nav" >
+                <ExitToAppIcon style={{color: '#b32800'}} fontSize="medium" />
+                <p onClick={Logout}>Logout</p>
+                </div>
+                :
+                ''
+            }
             </div>
+
 
             {/* <div>
             <MenuIcon fontSize="large" style={{fontSize:'30px',cursor:'pointer', marginLeft: '100px'}} onClick={OpenNav} />
